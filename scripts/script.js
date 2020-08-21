9b821636ea0c2241a44afd8eb2b79a442f5d1972
AlexKaryasov <hypotalamus95@gmail.com>
2020-08-21 22:33:08 +0400

    Point 5 is done: two numbers are operated

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,14 @@
 //global declarations
-let displayButtons = document.querySelectorAll('.display-button');
+let numberButtons = document.querySelectorAll('.number-button');
+let orepationButtons = document.querySelectorAll('.operation-button');
+let resultButton = document.querySelector('.result-button');
 let display = document.querySelector("#display")
-let operation;
+
+const calculator = {
+    operation: "",
+    operands: [],
+    lastInputCharacter: ""
+}
 
 
 const add = (a, b) => a + b;
@@ -9,11 +16,13 @@ const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
 
-const operate = (operator, a, b) => {
+const operate = (calculator) => {
     let result;
-    switch (operator) {
+    let a = calculator.operands[0];
+    let b = calculator.operands[1];
+    switch (calculator.operation) {
         case "+":
-            result = add(a, b);
+            result = add(+a, +b);
             break;
         case "-":
             result = subtract(a, b);
@@ -26,18 +35,45 @@ const operate = (operator, a, b) => {
             break;
     }
 
+    console.log(calculator.operator)
+    console.log(result);
     return result;
 }
 
-const clickNunmberButton = (event) => {
+const clickDisplayButton = (event) => {
     display.textContent += event.target.innerHTML;
+    calculator.lastInputCharacter = event.target.innerHTML;
+}
+
+const clickNumberButton = (event) => {
+    let numberRegex = /[0-9]/
+    if (calculator.operands[calculator.operands.length - 1] && numberRegex.test(calculator.lastInputCharacter)) {
+        calculator.operands[calculator.operands.length - 1] += event.target.innerHTML;
+    } else {
+        calculator.operands.push(event.target.innerHTML);
+    }
 }
 
 const clickOrepationButton = (event) => {
-    display.textContent += event.target.innerHTML;
-    operation = event.target.innerHTML;
-}l
+    calculator.operation = event.target.innerHTML;
+}
 
-displayButtons.forEach((displayButton) => {
-    displayButton.addEventListener("click", clickNunmberButton);
+const clickResultButton = (event) => {
+    display.textContent = operate(calculator)
+}
+
+numberButtons.forEach((displayButton) => {
+    displayButton.addEventListener("click", (event) => {
+        clickNumberButton(event);
+        clickDisplayButton(event);
+    });
 });
+
+orepationButtons.forEach((displayButton) => {
+    displayButton.addEventListener("click", (event) => {
+        clickOrepationButton(event);
+        clickDisplayButton(event);
+    });
+});
+
+resultButton.addEventListener("click", clickResultButton);
